ad95995792ef348ebf140d3b23676df09b9c01d0
Gowrish <sarangowrishvelisetty@gmail.com>
2026-02-07 20:37:13 +0530

    final commit

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="header">
-      <h1>Taskify</h1>
+      <h1>TaskBuddy</h1>
       <p>Plan smart. Work better.</p>
     </header>
   );
